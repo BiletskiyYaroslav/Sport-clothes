8fc52b56596181639d113cd380261a172aff5915
BiletskiyYaroslav <beketskiy.yroslav@gmail.com>
2024-12-14 21:11:06 +0200

    final cards

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 function getProductHtml(product) {
     return `
-      <div class="card" style="width: 18rem;">
-      <img src="Images/${product.image}" class="card-img" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">${product.title}</h5>
-        <p class="card-text">${product.description}</p>
-        <a href="#" class="btn btn-primary">${product.price}$</a>
+      <div class="card">
+      <img src="Images/${product.image}" class="card-img" alt="..." >
+      <div class="card-body" >
+        <h5 class="card-title" >${product.title}</h5>
+        <p class="card-text" >${product.description}</p>
+        <div class= "card-down">
+            <span class="card-price" >Ціна ${product.price}$</</span>
+            <a href="#" class="buy-button" >Купить</a>
+        </div>
       </div>
     </div>
     `
@@ -26,3 +29,34 @@ function getProductHtml(product) {
     }
   })
   
+// Функция для отображения товаров в DOM
+function displayProducts(products) {
+  const productsContainer = document.querySelector('.catalog');
+  productsContainer.innerHTML = ''; // очищаем контейнер перед добавлением новых товаров
+
+  products.forEach(function(product) {
+      productsContainer.innerHTML += getProductHtml(product);
+  });
+}
+
+// Функция для фильтрации товаров по поисковому запросу
+function filterProducts(query, products) {
+  const filteredProducts = products.filter(product => 
+      product.title.toLowerCase().includes(query.toLowerCase()) || 
+      product.description.toLowerCase().includes(query.toLowerCase())
+  );
+  displayProducts(filteredProducts);
+}
+
+// Обработчик поиска
+document.getElementById('search-input').addEventListener('input', function(event) {
+  const query = event.target.value;
+  getProducts().then(function(products) {
+      filterProducts(query, products);
+  });
+});
+
+// Загружаем и отображаем все товары при загрузке страницы
+getProducts().then(function(products) {
+  displayProducts(products);
+});
